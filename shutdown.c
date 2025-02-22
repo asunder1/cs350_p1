7b3ce86072fc0115e28e0a5351ef333cfd022843
Alex Sunderhaft <asunder1@binghamton.edu>
2025-02-22 16:29:24 -0500

    added shutdown syscall

--- shutdown.c
+++ shutdown.c
@@ -5,12 +5,13 @@
 /* ATTENTION: to ensure correct compilation of the base code, 
    stub functions for the system call user space wrapper functions are provided. 
    REMEMBER to disable the stub functions (by commenting the following macro) to 
-   allow your implementation to work properly. */
-#define STUB_FUNCS
+   allow your implementation to work properly.
+*/
+
+//#define STUB_FUNCS
 #ifdef STUB_FUNCS
 void shutdown(void) {}
 #endif
-
 
 /* IMPORTANT INSTRUCTION: the code below should not be changed. 
     Failure to follow this instruction will lead to zero point 
